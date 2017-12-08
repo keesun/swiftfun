c03c6de58e76b4b067486bd8778cc77d64c8b33e
Keesun Baik <whiteship2000@gmail.com>
2017-12-07 22:46:51 -0800

    First Commit

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var buttonCount = 0
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        buttonCount += 1;
+        print(buttonCount)
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            titleLabel.text = "Hello Sophia"
+        }
+        
+        if buttonCount >= 15 {
+            view.backgroundColor = UIColor.green
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
